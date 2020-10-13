e7e04aec216e6cdd8c4664e78d67a9239b003005
Ahmed Ramadan <eng.mido.sharaf1@gmail.com>
2020-10-13 18:42:26 +0200

    full pipeline deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Ahmed :)'));
+app.get('/', (req, res) => res.send('Hello Ahmed :) This is full pipeline deployment web app'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
